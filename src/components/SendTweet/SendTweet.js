1fe8f5285f1f50d252e763fc0e3e11bf2743c06e
Jose Jaime Gtz <jose_jaime@live.com.mx>
2021-01-11 18:27:18 -0600

    Seccion 7: 54. LocalStorage

---
+++ src/components/SendTweet/SendTweet.js
@@ -0,0 +1,48 @@
+import React, { useState } from "react";
+import "./SendTweet.scss";
+import { Fab } from "@material-ui/core";
+import AddIcon from "@material-ui/icons/Add";
+import ModalContainer from "../ModalContainer";
+import FormSendTweet from "../FormSendTweet";
+import moment from "moment";
+import { TWEETS_STORAGE } from "../../utils/constants";
+
+export default function SendTweet() {
+  const [isOpenModal, setIsOpenModal] = useState(false);
+  const openModal = () => {
+    setIsOpenModal(true);
+  };
+  const closeModal = () => {
+    setIsOpenModal(false);
+  };
+  const sendTweet = (event, formValue) => {
+    event.preventDefault();
+    const { name, tweet } = formValue;
+    let allTweetsArray = [];
+    if (!name || !tweet) {
+      console.log("Faltan llenar uno de los campos!");
+    } else {
+      formValue.time = moment();
+      allTweetsArray.push(formValue);
+      localStorage.setItem(TWEETS_STORAGE, JSON.stringify(allTweetsArray));
+      console.log("Tweet enviado correctamente!");
+      closeModal();
+    }
+    allTweetsArray = [];
+  };
+  return (
+    <div className="send-tweet">
+      <Fab
+        className="send-tweet__open-modal"
+        color="primary"
+        aria-label="add"
+        onClick={openModal}
+      >
+        <AddIcon />
+      </Fab>
+      <ModalContainer isOpenModal={isOpenModal} closeModal={closeModal}>
+        <FormSendTweet sendTweet={sendTweet} />
+      </ModalContainer>
+    </div>
+  );
+}
